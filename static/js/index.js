41fb8cacdb1882ade776e833ed3760e42eb5f8b5
FILLIOL Arnaud <arnaud.filliol@ca-gip.fr>
2022-04-04 16:22:26 +0200

    game setup

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,58 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function (event) {
 
+    const rockBtn = document.querySelector('#rock');
+    const paperBtn = document.querySelector('#paper');
+    const scissorsBtn = document.querySelector('#scissors');
+    const lizardBtn = document.querySelector('#lizard');
+    const spockBtn = document.querySelector('#spock');
+    const computerOptions = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+
+    const rpslvEnum = {
+        rock: 0,
+        paper: 1,
+        scissors: 2,
+        lizard: 3,
+        spock: 4
+    };
+
+    const scheme = [
+        [0, -1, 1, 1, -1],
+        [1, 0, -1, -1, 1],
+        [-1, 1, 0, 1, -1],
+        [-1, 1, -1, 0, 1],
+        [1, -1, 1, -1, 0]
+    ];
+
+    const getResult = function (id) {
+        const computerMove = computerOptions[Math.floor(Math.random() * computerOptions.length)];
+        const result = scheme[rpslvEnum[id]][rpslvEnum[computerMove]];
+        switch (result) {
+            case 0:
+                return "It's a tie";
+            case 1:
+                return "Player one had won";
+            case -1:
+                return "Player two had won";
+            default:
+                throw new Error("Something unexpected happened");
+        }
+    }
+
+    rockBtn.addEventListener('click', event => {
+        alert(getResult(rockBtn.id));
+    });
+    paperBtn.addEventListener('click', event => {
+        alert(getResult(paperBtn.id));
+    });
+    scissorsBtn.addEventListener('click', event => {
+        alert(getResult(scissorsBtn.id));
+    });
+    lizardBtn.addEventListener('click', event => {
+        alert(getResult(lizardBtn.id));
+    });
+    spockBtn.addEventListener('click', event => {
+        alert(getResult(spockBtn.id));
+    });
 
 
 });
